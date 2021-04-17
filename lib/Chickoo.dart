18a60dba97c892377e9d83662a67359cc5a4fbd5
Rayson L Dsz <72185590+Rayson-LD@users.noreply.github.com>
2021-04-17 12:37:08 +0530

    debugShowCheckedModeBanner: false updated

--- lib/Chickoo.dart
+++ lib/Chickoo.dart
@@ -4,6 +4,7 @@ class chickoo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+    debugShowCheckedModeBanner: false,
       home: new Scaffold(
         appBar: new AppBar(
           centerTitle: true,
